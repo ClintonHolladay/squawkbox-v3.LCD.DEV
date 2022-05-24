09fbca6e34ecf53ed060f8254a51e453bf70692a
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2022-05-24 16:46:02 -0500

    Update squawkbox_v3_LCD_0.ino
    // functional i2c screen for the first time
    // had a lot of issues with the screen flickering

--- squawkbox_v3_LCD_0.ino
+++ squawkbox_v3_LCD_0.ino
@@ -1,19 +1,7 @@
-/*LiquidCrystal
- LCD RS pin to digital pin 8
- LCD Enable pin to digital pin 7
- LCD D4 pin to digital pin 42
- LCD D5 pin to digital pin 44
- LCD D6 pin to digital pin 46
- LCD D7 pin to digital pin 48
- LCD R/W pin to ground
- LCD VSS pin to ground
- LCD VCC pin to 5V
- 10K resistor:pot
- ends to +5V and ground
- wiper to LCD VO pin (pin 3)
-*/
 #include <SD.h>
-#include <LiquidCrystal.h>
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h>
+
 //The following const int pins are all pre-run in the PCB:
 const int low1 = 5;  //to screw terminal
 const int low2 = 6;   //to screw terminal
@@ -22,10 +10,7 @@ const int hplcIN {14};
 const int hplcOUT {15};
 const int PLWCOoutletPin {17};
 const int SLWCOoutletPin {16};
-
-// LCD Mega pin layout for Squawk BreadBoard
-const int rs = 8, en = 7, d4 = 42, d5 = 44, d6 = 46, d7 = 48;
-
+//const int LCDFlickerDelay {3000};
 const int debounceInterval = 3000;  //to prevent false alarms from electrical noise.  
 //Setting this debounce too high will prevent the annunciation of instantaneous alarms like a bouncing LWC.
 
@@ -35,14 +20,16 @@ unsigned long difference2 {};
 unsigned long difference3 {};
 unsigned long difference4 {};
 unsigned long difference5 {};
+unsigned long ClearScreendifference {};
 unsigned long alarmTime {};
 unsigned long alarmTime2 {};
 unsigned long alarmTime3 {};
 unsigned long alarmTime4 {};
+unsigned long ClearScreenTime {};
 
 bool PLWCOSent{};
 bool SLWCOSent{};
-bool HWAlarmSent{0};
+bool HWAlarmSent{};
 bool hlpcSent{};
 bool alarm{};
 bool primaryCutoff{};
@@ -55,14 +42,14 @@ bool alarmSwitch = false;
 bool alarmSwitch2 = false;
 bool alarmSwitch3 = false;
 bool alarmSwitch4 = false;
-bool hlpcDisplay{};
+bool ClearScreenSwitch {};
 
-String PrimaryString {"Primary LowWater"};
-String SecondaryString {"2nd Low Water"};
+String PrimaryString {"Primary Low Water"};
+String SecondaryString {"Secondary Low Water"};
 String hlpcString {"High Limit"};
 String AlarmString {"FSG Alarm"};
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 File myFile;
 
 //=======================================================================
@@ -71,19 +58,21 @@ void setup() {
 
   Serial.begin(9600);
   Serial1.begin(19200);
-  Serial.println(F("This is squawkbox v3.LCD.0 sketch."));
-
-  // set up the LCD's number of columns and rows:
-  lcd.begin(20, 5);
+  Serial.println(F("This is squawkbox V3.LCD.0 sketch."));
+  lcd.init();
+  lcd.backlight();
+  lcd.begin(20, 4); // initialize LCD screen (columns, rows)
+  lcd.setCursor(2, 1);
   lcd.print("AB3D Squawk Box");
   delay(2000);
+  lcd.clear();
+  lcd.noBacklight();
   pinMode(low1, INPUT);
   pinMode(low2, INPUT);
   pinMode(alarmPin, INPUT);
   pinMode(hplcIN, INPUT);
   pinMode(hplcOUT, INPUT);
   pinMode(PLWCOoutletPin, INPUT);
-  delay(10);
   Serial.println(F("Setup() Function complete. Entering Main Loop() Function"));
 }
 //=========================================================================
@@ -97,9 +86,24 @@ void loop()
   hlpcNC = digitalRead(hplcOUT);
   PLWCOoutlet = digitalRead(PLWCOoutletPin);
   SLWCOoutlet = digitalRead(SLWCOoutletPin);
-  currentMillis = millis();
+  currentMillis = millis(); //??why isnt this used in the funtcions??
 
-  print_alarms();
+  if (ClearScreenSwitch == false)
+    {
+      ClearScreenTime = millis(); 
+      ClearScreenSwitch = true;
+    }
+    
+  ClearScreendifference = millis() - ClearScreenTime;
+
+  if ( ClearScreendifference >= debounceInterval)
+    {
+      lcd.clear();
+      print_alarms();
+      ClearScreendifference = 0;
+      ClearScreenSwitch = false;
+      //ClearScreenTime = 0;
+    }
   primary_LW();
   secondary_LW();
   Honeywell_alarm();
@@ -110,176 +114,108 @@ void print_alarms()
 {
   if (!PLWCOSent && !SLWCOSent &&!HWAlarmSent &&!hlpcSent)
   {
-    lcd.clear();
+    //lcd.clear();
+    lcd.noBacklight();
+    lcd.setCursor(6, 1);
     lcd.print("No Alarm");
-    delay(30);
   }
-  else if (PLWCOSent && SLWCOSent && hlpcSent &&HWAlarmSent )
+  else if (PLWCOSent && SLWCOSent && hlpcSent && HWAlarmSent )
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(SecondaryString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(SecondaryString);
-     lcd.setCursor(0, 1);
+     lcd.setCursor(0, 2);
      lcd.print(hlpcString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(hlpcString);
-     lcd.setCursor(0, 1);
-     lcd.print(AlarmString);
-     delay(2000); 
-     lcd.clear();
-     lcd.print(AlarmString);
-     lcd.setCursor(0, 1);
-     lcd.print(PrimaryString);
-     delay(2000); 
+     lcd.setCursor(0, 3);
+     lcd.print(AlarmString); 
   }
   else if (SLWCOSent && hlpcSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(SecondaryString);
      lcd.setCursor(0, 1);
      lcd.print(hlpcString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(hlpcString);
-     lcd.setCursor(0, 1);
-     lcd.print(AlarmString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(AlarmString);
-     lcd.setCursor(0, 1);
-     lcd.print(SecondaryString);
-     delay(2000);  
+     lcd.setCursor(0, 2);
+     lcd.print(AlarmString); 
   }
   else if (PLWCOSent && SLWCOSent && hlpcSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(SecondaryString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(SecondaryString);
-     lcd.setCursor(0, 1);
-     lcd.print(hlpcString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(hlpcString);
-     lcd.setCursor(0, 1);
-     lcd.print(PrimaryString);
-     delay(2000);  
+     lcd.setCursor(0, 2);
+     lcd.print(hlpcString); 
   }
   else if (PLWCOSent && SLWCOSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(SecondaryString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(SecondaryString);
-     lcd.setCursor(0, 1);
-     lcd.print(AlarmString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(AlarmString);
-     lcd.setCursor(0, 1);
-     lcd.print(PrimaryString);
-     delay(2000);  
+     lcd.setCursor(0, 2);
+     lcd.print(AlarmString);  
   }
   else if (PLWCOSent && hlpcSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(hlpcString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(hlpcString);
-     lcd.setCursor(0, 1);
-     lcd.print(AlarmString);
-     delay(2000);
-     lcd.clear();
-     lcd.print(AlarmString);
-     lcd.setCursor(0, 1);
-     lcd.print(PrimaryString);
-     delay(2000);  
+     lcd.setCursor(0, 2);
+     lcd.print(AlarmString); 
   }
   else if (PLWCOSent && SLWCOSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(SecondaryString);
-     delay(30); 
   }
   else if (PLWCOSent && hlpcSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(hlpcString);
-     delay(30); 
   }
   else if (PLWCOSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(PrimaryString);
      lcd.setCursor(0, 1);
      lcd.print(AlarmString);
-     delay(30); 
   }
   else if (SLWCOSent && hlpcSent)
   {
-     lcd.clear();
      lcd.print(SecondaryString);
      lcd.setCursor(0, 1);
      lcd.print(hlpcString);
-     delay(30); 
   }
   else if (SLWCOSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(SecondaryString);
      lcd.setCursor(0, 1);
      lcd.print(AlarmString);
-     delay(30); 
   }
   else if (hlpcSent && HWAlarmSent)
   {
-     lcd.clear();
      lcd.print(hlpcString);
      lcd.setCursor(0, 1);
      lcd.print(AlarmString);
-     delay(30); 
   }
   else if (PLWCOSent)
   {
-    lcd.clear();
+    lcd.backlight();
     lcd.print(PrimaryString);
-    delay(30);
   }
   else if (SLWCOSent)
   {
-    lcd.clear();
+    lcd.backlight();
     lcd.print(SecondaryString);
-    delay(30);
   }
   else if (HWAlarmSent)
   {
-    lcd.clear();
+    lcd.backlight();
     lcd.print(AlarmString);
-    delay(30);
   }
   else if (hlpcSent)
   {
-    lcd.clear();
+    lcd.backlight();
     lcd.print(hlpcString);
-    delay(30);
   }
 }
 
@@ -336,7 +272,6 @@ void secondary_LW()
       Serial.println(F("Secondary low water.  Sending message."));
       // send message code
       Serial.println(F("message sent or simulated"));
-      delay(10);
       SLWCOSent = 1;
       difference2 = 0;
       alarmSwitch2 = false;
