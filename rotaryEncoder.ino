1ad56c3705a1ca115754227ff7f9b4c474fb33b5
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2022-07-07 07:03:24 -0500

    Update rotaryEncoder.ino
    formatted variable initialization.
    added good comments.
    cleaned code.
    removed "digitalRead(pushButton) == LOW" from 2nd if statment and used LCDTimerSwitch instead so that the user doesnt have to be holding the 
         button during this execution.

--- rotaryEncoder.ino
+++ rotaryEncoder.ino
@@ -1,16 +1,15 @@
 
-const int pushButton = 5;
-const int encoder0PinA = 4;
-const int encoder0PinB = 3;
+const int pushButton {5};
+const int encoderPinA {4};
+const int encoderPinB {3};
 static bool userInput = false; // when true display EEPROM faults
                               // when false display current faults
 
 void setup() 
 {
-  pinMode (encoder0PinA, INPUT);
-  pinMode (encoder0PinB, INPUT);
+  pinMode (encoderPinA, INPUT);
+  pinMode (encoderPinB, INPUT);
   pinMode (pushButton, INPUT_PULLUP);
-  //digitalWrite (pushButton, HIGH);
   Serial.begin (9600);
 }
 
@@ -21,44 +20,47 @@ void loop()
 
 void LCDDisplayEEPROM()
 {
-  static int LCDscreenPage = 0;
-  static int encoder0PinALast = LOW;
-  static int n = LOW;
+  static int LCDscreenPage {};
+  static int encoderPinALast {LOW};
+  static int n {LOW};
   static unsigned long LCDdebounce{};
   static int debounceDelay{350};
-  static bool LCDTimerSwitch = false;
-  
+  static bool LCDTimerSwitch {false};
+
+  // Using a timer to latch the momentary user input into a constant ON or OFF position
+  // LCDTimerSwitch is used to allow main loop to continue and LCDdebounce to only be set to millis() once per pushButton push
   if(digitalRead(pushButton) == LOW && LCDTimerSwitch == false)
   {
     LCDdebounce = millis();
     LCDTimerSwitch = true;
   }
-  if (digitalRead(pushButton) == LOW && (millis() - LCDdebounce) > debounceDelay)
+  /*  Once userInput has been recieved and the debounce time has passed we ! the userInput bool and turn 
+   *  off the LCDTimerSwitch to stop running through the timer code until a new userinput is recieved.*/
+  if (LCDTimerSwitch && (millis() - LCDdebounce) > debounceDelay)
   {
-    if(userInput)
-    {
-      Serial.println ("User Input Recieved OFF");
-    }
-    else Serial.println ("User Input Recieved ON");
     userInput = !userInput;
     LCDTimerSwitch = false;
+    if(userInput)Serial.println ("User Input Recieved ON");
+    else Serial.println ("User Input Recieved OFF");
   }
+  /*  The userInput bool is used as a latch so that the LCD screen continues to display what the user wants until the pushButton 
+   *  is activated again. When the above timer algorithm has been satisfied the squawk will begin tracking rotational rotary 
+   *  encoder input from the user in order to traverse and LCD display the EEPROM fault data.*/
   if(userInput)
   {
-    n = digitalRead(encoder0PinA);
-  if ((encoder0PinALast == LOW) && (n == HIGH)) 
-  {
-    if (digitalRead(encoder0PinB) == LOW) 
+    n = digitalRead(encoderPinA);
+    if ((encoderPinALast == LOW) && (n == HIGH)) 
     {
-      LCDscreenPage--; //LCD.print statments from EEPROM here
-    } 
-    else 
-    {
-      LCDscreenPage++; //LCD.print statments from EEPROM here
+      if (digitalRead(encoderPinB) == LOW) 
+      {
+        LCDscreenPage--; //LCD.print statments from EEPROM here
+      } 
+      else 
+      {
+        LCDscreenPage++; //LCD.print statments from EEPROM here
+      }
+      Serial.println (LCDscreenPage);
     }
-    Serial.println (LCDscreenPage);
-  }
-  encoder0PinALast = n;
-  }
-  
+    encoderPinALast = n;
+  } 
 }
