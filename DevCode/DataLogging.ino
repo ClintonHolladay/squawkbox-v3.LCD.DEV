a2dafcc24096cfba5d11ccd1a67aab38645a2231
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2022-07-14 09:54:16 -0500

    Update DataLogging.ino
    1st complete draft

--- DevCode/DataLogging.ino
+++ DevCode/DataLogging.ino
@@ -1,9 +1,11 @@
 #include <SD.h>   
+#include <RTClib.h>
+#include <LibPrintf.h>
 
 void setup() 
 {
   Serial.begin(9600);
-  boot_SDcard();  
+  boot_SD();  
 }
 
 void loop() 
@@ -19,13 +21,15 @@ void Data_Logger(const char& FAULT[])
   {
     dataFile.print(FAULT);
     dataFile.print(",");
+    dataFile.print(now.unixtime());
+    dataFile.print(",");
     dataFile.print(now.year());  
     dataFile.print(",");      
     dataFile.print(now.month());  
     dataFile.print(",");
     dataFile.print(now.day());  
     dataFile.print(",");
-    dataFile.print(now.hour());  
+    dataFile.print(now.hour());   
     dataFile.print(",");
     dataFile.print(now.minute());  
     dataFile.print(",");
@@ -37,19 +41,54 @@ void Data_Logger(const char& FAULT[])
   } 
   else
   {
-    printf("Data Log failed");
+    printf("Data Log failed.\n");
   }
 }
 
-void boot_SDcard() {
-  // see if the card is present and can be initialized:
-  if (!SD.begin(10)) {
-    printf("Card failed or not present");
-    return;
+void boot_SD() //see if the card is present and can be initialized
+{
+  if (!SD.begin(10)) 
+  {
+    printf("SD Card failed or not present.\n");
   }
-  File dataFile = SD.open("DataLogger.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println("Fault,Year,Month,Day,Hour,Minute,Second");
-    dataFile.close();
+  else
+  {
+    File dataFile = SD.open("DataLogger.txt", FILE_WRITE);
+    if (dataFile) 
+    {
+      if(!dataFile.position())
+      {
+        dataFile.println("Fault,Year,Month,Day,Hour,Minute,Second");
+        dataFile.close();
+        printf("SD Prior initialization recognition complete.\n");
+      }
+      else
+      {
+        DateTime now = rtc.now();
+        dataFile.print("SystemRESET");
+        dataFile.print(",");
+        dataFile.print(now.unixtime());
+        dataFile.print(",");
+        dataFile.print(now.year());  
+        dataFile.print(",");      
+        dataFile.print(now.month());  
+        dataFile.print(",");
+        dataFile.print(now.day());  
+        dataFile.print(",");
+        dataFile.print(now.hour());  
+        dataFile.print(",");
+        dataFile.print(now.minute());  
+        dataFile.print(",");
+        dataFile.print(now.second());  
+        dataFile.print(",");
+        dataFile.println();  //End of Row move to next row
+        dataFile.close();    //Close the file
+        printf("SD SystemRESET Data Log Complete.\n");
+      }
+    }
+    else
+    {
+      printf("SD Prior initialization recognition OR SystemRESET Data Log Failed.\n");
+    }
   }
 }
